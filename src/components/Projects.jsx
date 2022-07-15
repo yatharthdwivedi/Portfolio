2fa0800ae72a63d2953e41dd9eb63ac94669cf1b
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-16 01:27:20 +0530

    p

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import mern from '../assets/mern.png';
 import Keeper from '../assets/Keeper.png'
 import booking from '../assets/Booking.png'
+import Journal from '../assets/Journal.png'
 
 const Projects = () => {
   return (
@@ -28,12 +29,12 @@ const Projects = () => {
                 MERN Stack Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://thawing-inlet-57545.herokuapp.com/login'>
+                <a target="_blank"href='https://thawing-inlet-57545.herokuapp.com/login'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/yatharthdwivedi/mern-app'>
+                <a target="_blank"href='https://github.com/yatharthdwivedi/mern-app'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -51,12 +52,12 @@ const Projects = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a  href='https://main.d1tny2i6pql7oe.amplifyapp.com/'>
+                <a target="_blank" href='https://main.d1tny2i6pql7oe.amplifyapp.com/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/yatharthdwivedi/Keeper-app'>
+                <a target="_blank"href='https://github.com/yatharthdwivedi/Keeper-app'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -75,12 +76,36 @@ const Projects = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://frolicking-cannoli-afeb39.netlify.app/'>
+                <a target="_blank"href='https://frolicking-cannoli-afeb39.netlify.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/yatharthdwivedi/booking'>
+                <a target="_blank"href='https://github.com/yatharthdwivedi/booking'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+            <div
+            style={{ backgroundImage: `url(${Journal})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a target="_blank"href='https://frolicking-cannoli-afeb39.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a target="_blank"href='https://github.com/yatharthdwivedi/booking'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
