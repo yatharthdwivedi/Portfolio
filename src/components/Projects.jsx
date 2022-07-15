aecab093e6f90e092a1d9e4d035fb4398a3bccff
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-16 02:38:44 +0530

    a

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -97,7 +97,7 @@ const Projects = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+              EJS with MongoDB Application
               </span>
               <div className='pt-8 text-center'>
                 <a target="_blank"href='https://frolicking-cannoli-afeb39.netlify.app/'>
