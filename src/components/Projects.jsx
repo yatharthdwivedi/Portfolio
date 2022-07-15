a51fc19faefaf4e434c51111d95968da94e773e9
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-15 20:58:23 +0530

    change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import mern from '../assets/mern.png';
 import Keeper from '../assets/Keeper.png'
-import booking from '../assets/Bnooking.png'
+import booking from '../assets/Booking.png'
 
 const Projects = () => {
   return (
@@ -112,29 +112,7 @@ const Projects = () => {
             </div>
           </div>
             {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${mern})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+         
           <div
             style={{ backgroundImage: `url(${Keeper})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
